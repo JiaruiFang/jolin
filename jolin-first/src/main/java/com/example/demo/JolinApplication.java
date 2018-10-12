1abfe939cf2c893c09daaeb49f80475ccab473d8
JiaruiFang <fangjiarui520716@163.com>
2018-10-12 10:13:56 +0800

    初识springboot，学习资料大放送

---
+++ jolin-first/src/main/java/com/example/demo/JolinApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+
+@SpringBootApplication
+public class JolinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JolinApplication.class, args);
+	}
+	
+}
